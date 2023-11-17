96f13796b458aeeee1f14ef9a3828ab65b8e4335
AgusIG <agusig026@gmail.com>
2023-11-17 14:30:22 -0300

    comentario

--- src/routes/nosotros.routes.js
+++ src/routes/nosotros.routes.js
@@ -1,5 +1,8 @@
-import { Router } from "express";
+/*Como no hay manejo de datos en la pagina de "nosotros" por ahora, 
+el endpoint de la misma aun esta definido, pero el archivo esta creado 
+para que en el futuro esto cambie, se ahorren unos pasos*/
 
+import { Router } from "express";
 
 const router = Router();
 
