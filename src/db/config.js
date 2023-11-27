0d2ad00bc28aa2e670017097a53492deca596524
bruno <brunoruiz050@gmail.com>
2023-11-27 17:26:21 -0300

    fix

--- src/db/config.js
+++ src/db/config.js
@@ -5,9 +5,9 @@ import { Sequelize } from 'sequelize';
 //que tipo de base de dato es
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'process.env.DB_NAME',
+    'process.env.DB_USER',
+    'process.env.DB_PASSWORD',
     {
         host: 'database-practicas.c5mmp0kpjhya.us-east-2.rds.amazonaws.com',
         dialect: 'mysql'
