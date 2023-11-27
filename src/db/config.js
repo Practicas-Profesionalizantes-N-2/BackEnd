1d9540e8a5a8c3cdec7747ea92d20cdcfd91723b
bruno <brunoruiz050@gmail.com>
2023-11-27 17:22:15 -0300

    fix

--- src/db/config.js
+++ src/db/config.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_PORT,
+        host: 'database-practicas.c5mmp0kpjhya.us-east-2.rds.amazonaws.com',
         dialect: 'mysql'
     }
 );
