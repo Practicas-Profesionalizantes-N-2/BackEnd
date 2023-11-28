7044fb13333172441092a3596a8c4cbe8bed1fb5
LucasSaez <lucassaez404@gmail.com>
2023-11-28 20:37:18 -0300

    cambio del cambio de los mensajes papi

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -14,17 +14,19 @@ router.get('/users', getAllUsers);
 
 //ruta para que el usuario pueda registrarse
 router.post('/register' , [
-    check('name', 'debe contener al menos 3 caracteres, ').isLength({min: 3}),
-    check('age', 'debe contener dos numeros').isLength({max: 2}),
-    check('email', 'el mail debe ser correcto').isEmail().not().isEmpty(),
+    check('name', 'el Nombre debe contener al menos 3 caracteres ').isLength({min: 3}),
+    check('lastname', 'el Apellido debe contener al menos 3 caracteres ').isLength({min: 3}),
+    check('age', 'Revise el campo edad').isLength({max: 3, min: 1}),
+    check('email', 'Revise el correo electronico').isEmail().not().isEmpty(),
+    check('password', 'La contraseña requiere como minimo 8 carácteres, una mayúscula, una minúscula, un número y un símbolo').isStrong().not().isEmpty(),
     validarCampos, verifySignUp, 
 ], createUser)
 
 
 //peticion POST login verificacion credenciales
 router.post('/login', [
-    check('email', 'El Email es requerido.').isEmail(),
-    check('password', 'La contraseña es requerida.').not().isEmpty(),
+    check('email', 'El Email o Contraseña es incorrecto.').isEmail(),
+    check('password', 'El Email o Contraseña es incorrecto.').not().isEmpty(),
     validarCampos, 
     loginAuthentication,
     loginUser,
